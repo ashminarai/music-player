c5f1747bbdb08bd055bacc9a74dcd4eb58abf314
ashminarai <raiashmina404@gmail.com>
2023-10-06 11:55:08 +0545

    updated css

--- src/components/Pages/Profile.jsx
+++ src/components/Pages/Profile.jsx
@@ -36,12 +36,13 @@ function Profile() {
         <Container>
             <div className={"Profile"}>
                 <div className="top-profile">
-                    <Avatar variant={"rounded"} src={require("../assets/img/avatar2.jpg")}
+                    <Avatar variant={"rounded"} src={require("../assets/img/avatar.jpeg")}
                             style={{width: "150px", height: "150px"}}>
                         VS
                     </Avatar>
                     <div className="profile-detail">
-                        <h3>Vishal Singh</h3>
+                        <h3>Ashmina Rai</h3>
+                        <p className='dev'>Frontend developer</p>
                         <span className={"profile-playlist"}>
                             <SideBarOptions className={"lib-sub"} Icon={PlaylistPlay}
                                             href={"/home/playlist/instrumental"} title={"Instrumental"}/>
