f12da2555b0b4ffa09f3d8201bc41751b297beba
ashminarai <raiashmina404@gmail.com>
2023-10-06 11:13:19 +0545

    updated icons button

--- src/components/fragment/Developer.jsx
+++ src/components/fragment/Developer.jsx
@@ -1,51 +1,73 @@
-import React from 'react';
-import '../assets/scss/Developer.scss';
-import {IconButton} from "@material-ui/core";
-import AvatarImage from "../assets/img/avatar.jpg";
-import {Facebook, Instagram, LinkedIn, Portrait, Twitter} from "@material-ui/icons";
+import React from "react";
+import "../assets/scss/Developer.scss";
+import { IconButton } from "@material-ui/core";
+// import AvatarImage from "../assets/img/avatar.jpeg";
+import {
+  Instagram,
+  LinkedIn,
+  Portrait,
+} from "@material-ui/icons";
 
 const Developer = () => {
-    return (
-        <div className={"Developer"}>
-            <h3 className={"Developer-head"}>Meet the developer</h3>
-            <div className="Developer-profile">
-                <div className="Developer-profileCard">
-                    <img src={AvatarImage} alt="Profile"/>
-                    <div className={"Card-details"}>
-                        <h3>Vishal Singh</h3>
-                        <p>Full Stack developer</p>
-                        <p>Competitive Coder</p>
-                    </div>
-                </div>
-                <div className="Developer-profileDetails">
-                    <p>A Computer Science and Engineering Student at Lovely Professional University, Punjab.</p>
-                    <p>Graduating in 2022 and looking for a responsible position to gain practical knowledge</p>
-                    <p>A full-stack web developer and a Competitive coder.</p>
-                    <p>I love designing fully responsive websites.</p>
-                    <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
-                    <p>And a blogger.</p>
-                    <div className="Card-btn">
-                        <IconButton target={"_blank"}  href={"https://www.facebook.com/vishal.kld"} title={"vishal.kld"}>
-                            <Facebook/>
-                        </IconButton>
-                        <IconButton target={"_blank"} href={"https://twitter.com/Vishal_kld"}  title={"Vishal_kld"}>
-                            <Twitter/>
-                        </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/vishalsingh-/"}  title={"vishalsingh-"}>
-                            <LinkedIn/>
-                        </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.instagram.com/vishalsingh023/"}  title={"vishalsingh023"}>
-                            <Instagram/>
-                        </IconButton>
-                        <IconButton target={"_blank"} href={"https://vishal.thetechnician.in/"}  title={"Web Portfolio"}>
-                            <Portrait/>
-                        </IconButton>
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div className={"Developer"}>
+      <h3 className={"Developer-head"}>Meet the developer</h3>
+      <div className="Developer-profile">
+        <div className="Developer-profileCard">
+          {/* <img src={AvatarImage} alt="Profile"/> */}
+          <div className={"Card-details"}>
+            <h3>Ashmina Rai</h3>
+            <p>Frontend developer</p>
+          </div>
         </div>
-    );
-}
+        <div className="Developer-profileDetails">
+          <p>
+            A highly motivated and skilled BSc. Computing graduate with a strong
+            foundation in computer science principles. I specialize in React and
+            Front end development. I am constantly seeking to expand my
+            knowledge and stay up-to- date with the latest technologies in the
+            field. I pride myself on my ability to learn quickly and adapt to
+            new challenges, making me an asset to any team and pride myself on
+            my expertise in a variety of areas, including time management,
+            software development, following good code architecture and SOLID
+            principles, Git version control, frontend, SQL, and good
+            communication skills. In other words, I'm a jack of all trades (but
+            hopefully not a master of none). Whether I'm tackling a new project
+            or solving a complex problem, I bring a strong set of skills and a
+            logical mindset to the table. And if that wasn't enough, I'm also a
+            pretty good multitasker - I just don't like to juggle while I'm
+            coding.
+          </p>
+
+          <div className="Card-btn">
+            
+           
+            <IconButton
+              target={"_blank"}
+              href={"https://www.linkedin.com/in/vishalsingh-/"}
+              title={"vishalsingh-"}
+            >
+              <LinkedIn />
+            </IconButton>
+            <IconButton
+              target={"_blank"}
+              href={"https://www.instagram.com/vishalsingh023/"}
+              title={"vishalsingh023"}
+            >
+              <Instagram />
+            </IconButton>
+            <IconButton
+              target={"_blank"}
+              href={"https://vishal.thetechnician.in/"}
+              title={"Web Portfolio"}
+            >
+              <Portrait />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Developer;
