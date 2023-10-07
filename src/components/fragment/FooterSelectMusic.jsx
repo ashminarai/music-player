61752c054415f2cca5d4888a72803357628b5cef
ashminarai <raiashmina404@gmail.com>
2023-10-07 13:48:44 +0545

    updated footer

--- src/components/fragment/FooterSelectMusic.jsx
+++ src/components/fragment/FooterSelectMusic.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import '../assets/scss/FooterSelectMusic.scss';
 import {ThemeContext} from "../../api/Theme";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function FooterSelectMusic() {
     const useStyle = useContext(ThemeContext);
